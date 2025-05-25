1e67134635aaae6a3d88bbe878ca9b0eb7293c7d
LDanas <danas.kozuchovskis@gmail.com>
2025-05-25 12:34:34 +0200

    Initial commit: setup Vue frontend

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+    <div id="app">
+      <BookList />
+    </div>
+  </template>
+  
+  <script>
+  import BookList from './components/BookList.vue';
+  
+  export default {
+    components: {
+      BookList,
+    },
+  };
+  </script>
+  
---
+++ src/components/BookList.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <h1>Book List</h1>
+    <ul>
+      <li v-for="book in books" :key="book.id">
+        {{ book.title }} by {{ book.author }} - Rating: {{ book.rating }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import BookService from '../services/BookService';
+
+export default {
+  data() {
+    return {
+      books: [],
+    };
+  },
+  created() {
+    BookService.getAllBooks().then((response) => {
+      this.books = response.data;
+    });
+  },
+};
+</script>
